c83584390f3f34358c90559ca09a476af1036fbc
sn0wvall <snowvall@protonmail.com>
2021-02-03 20:48:08 +0000

    ASCII Numbering for easier viewing

--- main.c
+++ main.c
@@ -1,32 +1,57 @@
-#include <stdio.h>
+#include <stdio.h> 		// Standard IO
+#include <stdlib.h> 		// System function
+
+// Define functions
+unsigned int sleep(unsigned int seconds);
 
 int main(){
 	// Define variables
-	unsigned int sleep(unsigned int seconds);
 
-	int intervalLength;
-	int intervalCount;
-
+	int intervalLength = 70;
+	int intervalCount = 2;
+   	
 	int i;
 	int j;
+	int minutesLeft;
+	int secondsLeft;
+	char intervalRemain[BUFSIZ];
 
 	// User enters length of each interval
 	printf("Enter the length in seconds of each interval: ");
-	scanf("%d", &intervalLength);
+	//scanf("%s", &intervalLength);
+	// Validate Input is not over 1 hour
+	if (intervalLength > 3599){
+		return 1;
+	}
 	printf("Interval Length set to: %ds\n", intervalLength);
-
+	
 	// User enters number of intervals to run through
 	printf("\nEnter the count of intervals: ");
-	scanf("%d", &intervalCount);
+	//scanf("%s", &intervalCount);
 	printf("Interval Count set to: %d\n", intervalCount);
 	
-	// Begin Counting
+	// Begin intervals
 
+	// Clear console
+	system("clear");
+
+	// Loop through each interval
 	for (i = 1; i <= intervalCount; ++i){
+
+		// Loop through each interval time
 		for (j = intervalLength; j > 0; --j){
-			printf("\r%d", j);
-			fflush(stdout);
-			sleep(1);
+			secondsLeft = j;
+			minutesLeft = 0;
+			do {
+				if (secondsLeft >= 60 && minutesLeft*60 < secondsLeft){
+					minutesLeft++;
+					secondsLeft = secondsLeft - 60;
+				}
+			} while (secondsLeft > 59);
+			snprintf(intervalRemain, sizeof(intervalRemain), "figlet '%d:%d'", minutesLeft, secondsLeft);			// Parse figlet command
+			system(intervalRemain);												// Execute figlet command
+			sleep(1);									
+			system("clear");												// Clear console
 		}
 	}	
 
