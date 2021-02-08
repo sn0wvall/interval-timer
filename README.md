8ed20da333043aebe679dd49a509049478a15a5c
sn0wvall <snowvall@protonmail.com>
2021-02-08 23:48:49 +0000

    Marginal Speed Gains

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # interval-timer
+
+big speed coming soon
+Current Drift Value / minute: 00:00.38
+Current Drift Value (2x5s): 00:00.034
--- main.c
+++ main.c
@@ -3,14 +3,12 @@
 #include <time.h>		// Clock Timing
 
 // Define functions
-unsigned int sleep(unsigned int seconds);
-int nanosleep(const struct timespec *req, struct timespec *rem);
 
 int main(){
 	// Define variables
 
-	int intervalLength = 60;
-	int intervalCount = 1;
+	int intervalLength = 5;
+	int intervalCount = 2;
 
 	int i;
 	int j;
@@ -26,45 +24,48 @@ int main(){
 	char intervalRemain[BUFSIZ];
 
 	// User enters length of each interval
-	printf("Enter the length in seconds of each interval:");
-	scanf("%d", &intervalLength);
+	//printf("Enter the length in seconds of each interval:");
+	//scanf("%d", &intervalLength);
 
 	// Validate Input is not over 1 hour
-	//if (intervalLength > 3599){
-	//	return 1;
-	//}
+	if (intervalLength >= 3599) { printf("\nInterval Length is over 3599"); return 1;}	
 	
 	// User enters number of intervals to run through
-	printf("\nEnter the count of intervals:");
-	scanf("%d", &intervalCount);
+	//printf("\nEnter the count of intervals:");
+	//scanf("%d", &intervalCount);
 
 	// Begin intervals
 
 	// Clear console
 	system("clear");
+	printf("\e[?25l");
 	
 	// Warm Up Period
-	for (j = 10; j >= 0; --j){
+	// for (j = 10; j >= 0; --j){
+
+	// 		clock_t formatBegin = clock();			// Begin Warmup Timing
+
+	// 		system("clear");				// Clear console
+	// 		secondsLeft = j;
+
+	// 		secondsLeftFormat[0] = '\0';
+
+	// 		// Format Seconds
+
+	// 		if (secondsLeft <= 9) { secondsLeftFormat[0] = '0'; }	
 			
-			system("clear");																// Clear console
+	// 		snprintf(intervalRemain, sizeof(intervalRemain), "figlet '00:%s%d'", secondsLeftFormat, secondsLeft);						// Parse figlet command
+	// 		system(intervalRemain);																// Execute figlet command
 
-			secondsLeft = j;
-
-			secondsLeftFormat[0] = '\0';
-
-			// Format Seconds
-
-			if (secondsLeft <= 9){
-				secondsLeftFormat[0] = '0';
-			}
+	// 		clock_t formatEnd = clock();															// End Timing
 			
-			snprintf(intervalRemain, sizeof(intervalRemain), "figlet '00:%s%d'", secondsLeftFormat, secondsLeft);						// Parse figlet command
-			system(intervalRemain);																// Execute figlet command
+	// 		formatTime = (double)(formatEnd - formatBegin) / CLOCKS_PER_SEC * 10000000;									// Adjust 1 second for CPU processing time
+	// 		timeSleep = 1000000 - formatTime * 2;
 
-			sleep(1);
-	}
+	// 		usleep(timeSleep);
+	// }
 
-	system("aplay -q ./ding.wav &");
+	// system("aplay -q ./ding.wav &");
 
 	// Loop through each interval
 	for (i = 1; i <= intervalCount; ++i){
@@ -91,20 +92,15 @@ int main(){
 
 			// Format Numbers
 
-			if (minutesLeft <= 9){
-				minutesLeftFormat[0] = '0';
-			}
-			if (secondsLeft <= 9){
-				secondsLeftFormat[0] = '0';
-			}
-			
+			if (minutesLeft <= 9) { minutesLeftFormat[0] = '0'; }	
+			if (secondsLeft <= 9) { secondsLeftFormat[0] = '0'; }	
+
 			snprintf(intervalRemain, sizeof(intervalRemain), "figlet '%s%d:%s%d'", minutesLeftFormat, minutesLeft, secondsLeftFormat, secondsLeft);		// Parse figlet command
 			system(intervalRemain);																// Execute figlet command
 
 			clock_t formatEnd = clock();															// End Timing
 			
-			formatTime = (double)(formatEnd - formatBegin) / CLOCKS_PER_SEC * 10000000;									// Adjust 1 second for CPU processing time
-			timeSleep = 1000000 - formatTime * 2;
+			timeSleep = 1000000 - (double)(formatEnd - formatBegin) / CLOCKS_PER_SEC * 10000000 * 3;							// Adjust 1 second for CPU processing time
 
 			usleep(timeSleep);
 		}
