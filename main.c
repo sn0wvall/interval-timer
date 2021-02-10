1c226e1bf5936c19cd5d2792134fefe4f316e0a2
sn0wvall <snowvall@protonmail.com>
2021-02-10 19:11:52 +0000

    Entire code rewritten into threads, reducing the time drift to a very small amount. Specific time drift and documentation coming after further testing

--- main.c
+++ main.c
@@ -2,24 +2,81 @@
 #include <stdlib.h> 		// System function
 #include <time.h>		// Clock Timing
 #include <pthread.h>		// Threading
+#include <unistd.h>
+
+int i;
+int j;
+
+pthread_t stopwatch;										// Define Stopwatch Thread
+pthread_t crier;										// Define Crier Thread
+pthread_t ding;											// Define Ding Thread
+
+unsigned int intervalLength = 3603;
+unsigned int intervalCount = 2;
+
+unsigned char hoursLeftFormat[] = " ";
+unsigned char minutesLeftFormat[] = " ";
+unsigned char secondsLeftFormat[] = " ";
+
+unsigned int hoursLeft;
+unsigned int minutesLeft; 
+unsigned int secondsLeft;
+
+unsigned char intervalRemain[BUFSIZ];
+
+unsigned int secondsLeftOld;
 
 // Define Threads
 
 void *timekeeper(void *args) {
+
 	// Loop through each interval
 	for (i = 1; i <= intervalCount; ++i){
 
 		// Loop through each interval time
 		for (j = intervalLength; j > 0; --j){
-			printf("\nintervalLength");
 			usleep(1000000);
 		}
-	}
+	}	
+
+	pthread_cancel(crier);
 
 	return NULL;
 }
 
-void *presenter(void *args) {
+void *announcer(void *args) {
+
+	for(;;){
+		secondsLeft = j;
+		usleep(100000);
+		if (secondsLeft != j){
+			secondsLeft = j;
+			minutesLeft = j / 60;
+			hoursLeft = minutesLeft / 60;
+
+			hoursLeftFormat[0] = '\0';
+			minutesLeftFormat[0] = '\0';
+			secondsLeftFormat[0] = '\0';
+
+			if (minutesLeft != 0) {secondsLeft = secondsLeft - 60 * minutesLeft;}
+			if (hoursLeft != 0) {minutesLeft = minutesLeft - 60 * hoursLeft;}
+
+			if (hoursLeft < 10) {hoursLeftFormat[0] = '0';}
+			if (minutesLeft < 10) {minutesLeftFormat[0] = '0';}
+			if (secondsLeft < 10) {secondsLeftFormat[0] = '0';}
+
+			system("clear");
+	 		snprintf(intervalRemain, sizeof(intervalRemain), "figlet '%s%d:%s%d:%s%d'", hoursLeftFormat, hoursLeft, minutesLeftFormat, minutesLeft, secondsLeftFormat, secondsLeft);						// Parse figlet command
+	 		system(intervalRemain);																// Execute figlet command
+		}
+	}
+	return NULL;
+}
+
+void *megaphone(void *args) {
+
+	// system("aplay -q ./ding.wav &");
+
 	return NULL;
 }
 
@@ -28,120 +85,42 @@ void *presenter(void *args) {
 int main(){
 
 	// Define variables
-
-	unsigned int intervalLength = 5;
-	unsigned int intervalCount = 2;
-
-	register int i;
-	register int j;
-	unsigned int minutesLeft;
-	unsigned int secondsLeft;
-	
-	struct timekeeperArgs {}
-	
+		
 	int stopwatchr;
+	int crierr;
+	int dingr;
 
-	double formatTime;
-	unsigned int timeSleep;
-	
-	unsigned char minutesLeftFormat[] = " ";
-	unsigned char secondsLeftFormat[] = " ";
-
-	unsigned char intervalRemain[BUFSIZ];
 
 	// User enters length of each interval
-	//printf("Enter the length in seconds of each interval:");
-	//scanf("%d", &intervalLength);
 
-	// Validate Input is not over 1 hour
-	if (intervalLength >= 3599) { printf("\nInterval Length is over 3599"); return 1;}	
-	
+	printf("Enter the length in seconds of each interval:");
+	scanf("%d", &intervalLength);
+
 	// User enters number of intervals to run through
-	//printf("\nEnter the count of intervals:");
-	//scanf("%d", &intervalCount);
-
+	
+	printf("\nEnter the count of intervals:");
+	scanf("%d", &intervalCount);
+	
 	// Begin intervals
 
 	// Clear console
 	printf("\e[?25l");
-	//system("clear");
-	
-	// Warm Up Period
-	// for (j = 10; j >= 0; --j){
-
-	// 		clock_t formatBegin = clock();			// Begin Warmup Timing
-
-	// 		system("clear");				// Clear console
-	// 		secondsLeft = j;
-
-	// 		secondsLeftFormat[0] = '\0';
-
-	// 		// Format Seconds
-
-	// 		if (secondsLeft <= 9) { secondsLeftFormat[0] = '0'; }	
-			
-	// 		snprintf(intervalRemain, sizeof(intervalRemain), "figlet '00:%s%d'", secondsLeftFormat, secondsLeft);						// Parse figlet command
-	// 		system(intervalRemain);																// Execute figlet command
-
-	// 		clock_t formatEnd = clock();															// End Timing
-			
-	// 		formatTime = (double)(formatEnd - formatBegin) / CLOCKS_PER_SEC * 10000000;									// Adjust 1 second for CPU processing time
-	// 		timeSleep = 1000000 - formatTime * 2;
-
-	// 		usleep(timeSleep);
-	// }
 
 	// system("aplay -q ./ding.wav &");
+	
+	// Start Threads
 
-	// Start Timekeeper
+	crierr = pthread_create(&crier, NULL, announcer, NULL);						// Spawn Announcer's Crier
+	stopwatchr = pthread_create(&stopwatch, NULL, timekeeper, NULL);				// Spawn Timekeeper's Stopwatch
+	dingr = pthread_create(&ding, NULL, megaphone, NULL);						// Spawn Megaphone's Ding
 
-	pthread_t stopwatch;
+	// Wait for all threads to finish
 
-	stopwatchr = pthread_create(&stopwatch, NULL, timekeeper, timekeeperArgs);
 	pthread_join(stopwatch, NULL);
+	pthread_join(crier, NULL);
+	pthread_join(ding, NULL);
 
-	// Loop through each interval
-	for (i = 1; i <= intervalCount; ++i){
-
-		// Loop through each interval time
-		for (j = intervalLength; j > 0; --j){
-			
-			clock_t formatBegin = clock();															// Begin Timing
-
-			//system("clear");																// Clear console
-
-			secondsLeft = j;
-			minutesLeft = 0;
-
-			minutesLeftFormat[0] = '\0';
-			secondsLeftFormat[0] = '\0';
-
-			while (secondsLeft > 59);{
-				if (secondsLeft >= 60 && minutesLeft*60 < secondsLeft){
-					minutesLeft++;
-					secondsLeft = secondsLeft - 60;
-				}
-			} 
-			// Format Numbers
-
-			if (minutesLeft <= 9) { minutesLeftFormat[0] = '0'; }	
-			if (secondsLeft <= 9) { secondsLeftFormat[0] = '0'; }	
-
-			snprintf(intervalRemain, sizeof(intervalRemain), "figlet '%s%d:%s%d'", minutesLeftFormat, minutesLeft, secondsLeftFormat, secondsLeft);		// Parse figlet command
-			system(intervalRemain);																// Execute figlet command
-
-			clock_t formatEnd = clock();															// End Timing
-			
-			timeSleep = 1000000 - (double)(formatEnd - formatBegin) / CLOCKS_PER_SEC * 10000000 * 3;							// Adjust 1 second for CPU processing time
-
-			usleep(timeSleep);
-		}
-
-	system("aplay -q ./ding.wav &");
-
-	}	
-
-	//printf("\e[?25h");
+	printf("\e[?25h");
 
 	return 0;
 }
